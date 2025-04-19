33cb0406b6657842898c9f08b9d6752aafd4ee81
polskapeeps <poitrkostka@gmail.com>
2025-04-18 23:55:37 -0400

    add createTray function to manage application tray icon and context menu

---
+++ tray.js
@@ -0,0 +1,26 @@
+const { Tray, Menu } = require('electron');
+const path = require('path');
+
+let tray = null;
+
+function createTray(mainWindow) {
+tray = new Tray(path.join(__dirname, 'build', 'icon.ico'));    const contextMenu = Menu.buildFromTemplate([
+        { Label: 'Passlock 1.0', 
+            click: () => {
+                mainWindow.show();
+            }
+        },
+        {
+            label: 'Quit',
+            click: () => {
+                require('electron').app.quit();
+            }
+        }
+    ]);
+
+    tray.setToolTip('PassLock');
+    tray.setContextMenu(contextMenu);
+}
+
+module.exports = createTray;
+  
